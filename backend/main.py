3e5e5fe6e35865b91316196d47f229fa096639e4
Uri Goodman <uri.goodman@gmail.com>
2022-11-18 13:55:49 +0200

    feat: build both

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.api:app", host="0.0.0.0",
+                port=int(os.environ['PORT']), reload=True)
