87d50731514dc3ad472576d11703c3545b2fa68c
Uri Goodman <uri.goodman@gmail.com>
2022-12-04 17:48:34 +0200

    feat: enabled debug mode on windows

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 import uvicorn
 import os
+import sys
+
+if sys.platform == 'win32':
+    os.environ['PORT'] = str(8000)
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0",
