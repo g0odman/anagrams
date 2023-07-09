e3477dcd823687c5a604e824f9c4907153e34902
Uri Goodman <uri.goodman@gmail.com>
2023-07-09 17:03:08 +0300

    fix: change default port

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
 import uvicorn
 import os
-import sys
-
-if sys.platform == 'win32':
-    os.environ['PORT'] = str(8000)
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0",
-                port=int(os.environ['PORT']), reload=True)
+    uvicorn.run(
+        "app.api:app", host="0.0.0.0", port=int(os.environ.get("PORT", 80)), reload=True
+    )
